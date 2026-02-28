ca56c6ce153adda7ff3cac3654b7ab559641e309
Rinali Pesakya <rinalipesakya@gmail.com>
2026-02-28 23:36:22 +0530

    UI of Login and Signup interacting with database

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";     
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+
+      <Link href= "/(auth)/signup">Signup </Link>
+      <Link href= "/(auth)">Login Page </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+  title: {
+    color: "blue"
+  },
+});
