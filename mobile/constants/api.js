1f1ede46bba50de81416556fc8161600f0efa2b8
Dinithi Gunathilake <e23117@eng.pdn.ac.lk>
2026-04-20 01:29:17 +0530

    Fix app.json

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://192.168.8.106:3000/api"; // Update with your backend server URL and port
+export const API_URL = "http://192.168.1.5:3000/api"; // Update with your backend server URL and port
